eaa0920da36e0d91ad7eaa3e1cf28163b36bfe75
niyuanwen1983 <13917943645@139.com>
2019-04-19 16:23:46 +0800

    登录、发送验证码接口调试

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,8 @@
 //login.js
+const md5 = require('../../assets/js/md5/md5.js')
+const CryptoJS = require('../../assets/js/aes/aes.js')
+const base64 = require('../../assets/js/base64/base64.js')
+
 //导入通用方法js
 const util = require('../../utils/util.js')
 
@@ -10,8 +14,7 @@ Page({
     'mobileno': '',
     'smscode': ''
   },
-  onLoad: function() {
-  },
+  onLoad: function() {},
   //获取用户输入的手机号
   mobilenoInput: function(e) {
     this.setData({
@@ -25,12 +28,38 @@ Page({
     })
   },
   login: function() {
+    let obj = wx.getStorageSync('user');
     if (util.isEmpty(this.data.mobileno)) {
       util.showToast('请输入手机号！')
     } else if (util.isEmpty(this.data.smscode)) {
       util.showToast('请输入验证码！')
     } else {
-      util.doApi(util.api1, {},this.successLogin, this.failLogin)
+      /*let params = {
+        'mobile': this.data.mobileno,
+        'smsCode': this.data.smscode
+      }*/
+      let timeM = new Date()
+
+      let dataJson = {
+        "mobile": this.data.mobileno,
+        "smsCode": this.data.smscode
+      }
+      let dataString = '{"mobile":"' + this.data.mobileno + '","smsCode":"' + this.data.smscode + '"}'
+
+      let sign = md5.hexMD5(dataString + util.signKey + timeM.getTime())
+
+      var paramData = util.Encrypt(dataString)
+
+      let params = {
+        "source": 2,
+        "token": obj.openid,
+        "sign": sign,
+        "data": paramData,
+        "deviceId": "",
+        "time": timeM.getTime()
+      }      
+
+      util.doApi(util.apiLogin, params, this.successLogin, this.failLogin)
     }
   },
   successLogin: function(res) {
@@ -38,5 +67,32 @@ Page({
   },
   failLogin: function(res) {
     console.log(res)
+  },
+
+  sendSms:function(){
+    let obj = wx.getStorageSync('user');
+    util.showToast('验证码已发送！')
+
+    let timeM = new Date()
+
+    let dataJson = {
+      "mobile": this.data.mobileno
+    }
+    let dataString = '{"mobile":"' + this.data.mobileno + '"}'
+
+    let sign = md5.hexMD5(dataString + util.signKey + timeM.getTime())
+
+    var paramData = util.Encrypt(dataString)
+
+    let params = {
+      "source": 2,
+      "token": obj.openid,
+      "sign": sign,
+      "data": paramData,
+      "deviceId": "",
+      "time": timeM.getTime()
+    }
+
+    util.doApi('/api/gps/sendSms.do', params, this.successLogin, this.failLogin)
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -25,7 +25,16 @@ const isEmpty = obj => {
   }
 }
 
-const api1 = 'https://www.easy-mock.com/mock/5c6c15b5ab815c130b4720c7/example/IndexList'
+//md5加密用
+const signKey = 'Vfd5Eh8HPvHIqpyno1OX'
+
+//aes加密用
+//const aesKey = '8n64jliKdUfZb0Ar4HzlCnYgGW1FuXS9'
+//const iv = '0102030405060708'
+
+//const api1 = 'https://www.easy-mock.com/mock/5c6c15b5ab815c130b4720c7/example/IndexList'
+const baseUrl = 'http://127.0.0.1:8080'
+const apiLogin = '/api/gps/login.do'
 
 /**
  * 显示toast
@@ -50,9 +59,10 @@ const doApi = (url, params, successFunction, failFunction) => {
   wx.showLoading({
     title: '加载中......',
   })
-  
+
   wx.request({
-    url: url,
+    url: baseUrl + url,
+    method:'POST',
     data: params,
     header: {
       'content-type': 'application/json'
@@ -72,10 +82,39 @@ const doApi = (url, params, successFunction, failFunction) => {
   })
 }
 
+const CryptoJS = require('../assets/js/aes/aes.js')
+var key = CryptoJS.enc.Utf8.parse("8n64jliKdUfZb0Ar4HzlCnYgGW1FuXS9");//十六位十六进制数作为秘钥
+var iv = CryptoJS.enc.Utf8.parse('0102030405060708');//十六位十六进制数作为秘钥偏移量
+
+//解密方法
+function Decrypt(word) {
+  var encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+  var srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+  var decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  var decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+  return decryptedStr.toString();
+}
+
+function Encrypt(word){
+  var srcs = CryptoJS.enc.Utf8.parse(word);
+  var encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  var hexStr = encrypted.ciphertext.toString().toUpperCase();
+
+  console.log('hexStr->' + hexStr);
+  var oldHexStr = CryptoJS.enc.Hex.parse(hexStr);
+  // 将密文转为Base64的字符串
+  var base64Str = CryptoJS.enc.Base64.stringify(oldHexStr);
+  console.log('base64Str->' + base64Str);
+  //这里根据需求返回 base64Str 或 hexStr(解密时有小小差别)
+  return base64Str;
+}
+
 module.exports = {
-  api1: api1,
+  signKey: signKey,
+  apiLogin: apiLogin,
   formatTime: formatTime,
   isEmpty: isEmpty,
   showToast: showToast,
-  doApi: doApi
+  doApi: doApi,
+  Encrypt: Encrypt
 }
